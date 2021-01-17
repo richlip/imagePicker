0335aa294f30a61c1f1d398eac439cd23c3335c1
richard <richlip@googlemail.com>
2021-01-17 18:07:46 +0100

    2 Buttons added

--- imagePicker/ViewController.swift
+++ imagePicker/ViewController.swift
@@ -9,9 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBAction func experiment(){
+    @IBAction func experiment(sender: UIButton){
+        let nextController = UIImagePickerController()
+        present(nextController, animated: true, completion: nil)
     }
     
+    @IBAction func experimentt(_ sender: Any) {
+
+        let image = UIImage()
+        let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        self.present(controller, animated: true, completion: nil)
+    }
+    
+    @IBAction func experimenttt(_ sender: Any) {
+
+        let controller = UIAlertController()
+        controller.title = "Test Alert"
+        controller.message = "This is a test"
+
+        let okAction = UIAlertAction(title: "ok", style: UIAlertAction.Style.default) { action in self.dismiss(animated: true, completion: nil)
+        }
+
+        controller.addAction(okAction)
+        self.present(controller, animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

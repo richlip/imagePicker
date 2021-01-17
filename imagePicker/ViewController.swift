f7a02e11f908bb43e5d29adf5328fec2083f72c8
richard <richlip@googlemail.com>
2021-01-17 17:37:50 +0100

    Update ViewController.swift

--- imagePicker/ViewController.swift
+++ imagePicker/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func experiment(){
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
